1b71e3963475c1e5042261da37bc49f3d72e746f
JaneSu <jianweisummer@gmail.com>
2020-11-16 10:43:56 +0800

    fix: 修复对 submenu 的支持

--- src/components/cloneItem.ts
+++ src/components/cloneItem.ts
@@ -1,6 +1,7 @@
 import { Children, cloneElement, ReactNode, ReactElement, isValidElement } from 'react';
 import { TriggerEvent } from '../types';
 import { Item } from './Item';
+import { Submenu } from './Submenu';
 
 export function cloneItem (
   children: ReactNode,
@@ -11,7 +12,11 @@ export function cloneItem (
     Children.toArray(children).filter(child => Boolean(child)),
     child => {
       if (!isValidElement(child)) { return child }
-      if (!(child.type === Item) && child.props.children) {
+      if (
+        child.type !== Item &&
+        child.type !== Submenu &&
+        child.props.children
+      ) {
         return cloneItem(child.props.children, props)
       }
       return cloneElement(child as ReactElement<any>, props)
